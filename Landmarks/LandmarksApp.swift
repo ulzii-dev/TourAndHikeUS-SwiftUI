4cfa2f1fbfde2d13b016bcdd69a9fd2b94952460
Saruul <s.saruululzii@gmail.com>
2024-01-13 01:03:54 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Saruululzii on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
